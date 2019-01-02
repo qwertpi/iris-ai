9deb75091dca3676969da2a4abbfd00eaaeb823c
qwertpi <qwertpi@users.noreply.github.com>
2019-01-02 18:26:14 +0000

    Added comments

--- predict.py
+++ predict.py
@@ -1,28 +1,38 @@
 import numpy as np
 from keras.models import load_model
 
+#loads the model
 model=load_model("model.h5")
 
+#gets the data we will be basing our prediction on
 sep_length=float(input("Enter the speal length in cm    "))
 sep_width=float(input("Enter the speal width in cm    "))
 pet_length=float(input("Enter the petal length in cm    "))
 pet_width=float(input("Enter the petal width in cm    "))
 
+#puts the data into the required format
 data=np.array([[sep_length, sep_width, pet_length, pet_width]])
+#makes a prediction
+#also turns it into a list
 prediction=list(model.predict(data)[0])
+#mimics argmax
 index=prediction.index(sorted(prediction)[-1])
 
+#tells the user which plant they have
 if index==1:
     print("Iris Setosa")
 elif index==2:
     print("Iris Versicolour")
 elif index==3:
     print("Iris Virginica")
+#in the event of a index 0 (no class) or anything else
 else:
+    #gives me debug info
     print("Error! Please raise this on GitHub with the following information")
     print(data)
     print(prediction)
-    print(model.predict(data))
+    print(index)
+    #uses the next most confident prediction
     print("Searching for alternative prediction")
     index=sorted(prediction)[1]
     if index==1:
--- train.py
+++ train.py
@@ -6,15 +6,16 @@ from sklearn.model_selection import train_test_split
 from keras import callbacks
 from keras.utils import to_categorical
 
+#loads and formats data
 data=np.loadtxt("iris.csv", delimiter=",")
 X = data[:,0:4]
 Y = data[:,4]
-print(X)
-print(Y)
 Y=to_categorical(Y)
 
+#train test splits
 x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.25)
 
+#model architecture
 model = Sequential()
 model.add(Dense(32,input_dim=4,activation='relu'))
 model.add(Dense(16,activation='relu'))
@@ -22,17 +23,20 @@ model.add(Dense(8,activation='relu'))
 model.add(Dense(5,activation='relu'))
 model.add(Dense(4,activation='softmax'))
 
+#custom callback to print learning rate
 class MyCallback(callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         print(K.eval(self.model.optimizer.lr))
-        
+
+#reduces learning rate when val_loss stagnates         
 ReduceLR=callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.9, verbose=0, cooldown=100, min_lr=0.0001)
+#saves the best model based on val_loss with the name of the val_acc rounded to 2 decimal places
 savebest=callbacks.ModelCheckpoint(filepath="checkpoint-{val_acc:.2f}.h5", monitor="val_loss", save_best_only=True)
 PrintLR=MyCallback()
 callbacks_list=[ReduceLR,PrintLR,savebest]
 
-
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+#trains the model for 10,000 epochs but it should probably be stopped earlier by a keyboard interrupt
 model.fit(x_train, y_train, epochs=10000, batch_size=64, validation_data=(x_test, y_test),callbacks=callbacks_list)
 
 from keras import backend as K
